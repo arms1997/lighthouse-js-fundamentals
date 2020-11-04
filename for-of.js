a82eea6117b63927fdc1e2cfca9f1a9c8f4f928c
arms1997 <armeen.hadizadeh@gmail.com>
2020-11-04 22:00:08 +0000

    commiting for-of.js

---
+++ for-of.js
@@ -0,0 +1,18 @@
+const io = require('console-read-write');
+
+async function main(){
+
+	let names = [];
+
+	for(let i = 0; i<5; i++){
+		io.write('Please insert one name');
+		const name = await io.read();
+		names.push(name);
+	}
+
+	for(let name of names){
+		console.log(`${name} is a lil baddie`);
+	}
+}
+
+main();	
